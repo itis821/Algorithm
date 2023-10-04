3c8a03f4b68dbee8c3a038bd90cfd90f248886c0
Lee Jisu <itis821@ajou.ac.kr>
2023-10-04 19:58:19 +0900

    BOJ2193

---
+++ dp/BOJ2193.java
@@ -0,0 +1,23 @@
+package dp;
+
+import java.io.*;
+
+class BOJ2193 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine()); // 1 <= N <=90
+
+        long[][] cnt = new long[N + 1][2];
+
+        cnt[1][0] = 0L;
+        cnt[1][1] = 1L;
+
+        for (int i = 2; i < N + 1; i++) {
+            cnt[i][0] = cnt[i - 1][0] + cnt[i - 1][1];
+            cnt[i][1] = cnt[i - 1][0];
+        }
+
+        System.out.println(cnt[N][0] + cnt[N][1]);
+
+    }
+}
