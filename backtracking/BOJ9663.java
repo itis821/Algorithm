3128ffb84cd20f07749208f9e9aa1a19c11e44f5
Lee Jisu <itis821@ajou.ac.kr>
2023-10-04 14:31:21 +0900

    BOJ9663

--- backtracking/BOJ9663.java
+++ backtracking/BOJ9663.java
@@ -1,64 +1,67 @@
 package backtracking;
 
-import java.util.*;
 import java.io.*;
 
 public class BOJ9663 {
-    public static int[][] board;
-    public static int count = 0;
-    public static int N;
+    static int cnt = 0;
+    static int[][] queens;
+    static int N;
+    static final int COL = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        board = new int[N][N];
+        N = Integer.parseInt(br.readLine()); // 1<= N < 15
+        queens = new int[N][1];
 
-        solution(0, 0, 0);
-        System.out.print(count);
+        // 한 행에는 한 개의 퀸만 놓을 수 있음
+        // 한 행씩 퀸을 놓을 위치 정하기
+        find(0);
+        System.out.println(cnt);
     }
 
-    public static void solution(int n, int row, int col) {
-        if (n == N) {
-            count++;
+    static void find(int line) {
+        if (line == N) {
+            cnt++;
             return;
         }
+        boolean gyupchu = false;
 
-        for (int i = row; i < N; i++) {
-            for (int j = col; j < N; j++) {
-                if (board[i][j] != 0) // 놓을 수 없는 자리
-                    continue;
+        for (int col = 0; col < N; col++) { // 퀸을 놓을 열 정하기
+            gyupchu = false;
 
-                fill(i, j);
-                solution(n + 1, i + 1, j + 1);
-                defill(i, j);
+            // 그 동안 퀸 놓은 곳 확인
+            for (int i = 0; i < line; i++) {
+
+                // 열 겹침
+                if (queens[i][COL] == col) {
+                    gyupchu = true;
+                    break;
+                }
+
+                // 대각 겹침
+                if (daegak(line, col, i, queens[i][COL])) {
+                    gyupchu = true;
+                    break;
+                }
+
             }
+
+            if (gyupchu)
+                continue;
+
+            // 퀸 놓기
+            queens[line][0] = col;
+            find(line + 1);
         }
     }
 
-    public static void fill(int row, int col) {
-
-        // 세로
-        for (int i = 0; i < N; i++) {
-            board[i][col] += 1;
+    static boolean daegak(int r1, int c1, int r2, int c2) {
+        if (Math.abs(r1 - r2) == Math.abs(c1 - c2)) {
+            return true;
+        } else {
+            return false;
         }
 
-        // 가로
-        for (int i = 0; i < N; i++) {
-            board[row][i] += 1;
-        }
-
-        for(int i = )
-        board[row][col] -= 1;
     }
 
-    public static void defill(int row, int col) {
-        for (int i = 0; i < N; i++) {
-            board[i][col] -= 1;
-        }
-
-        for (int i = 0; i < N; i++) {
-            board[row][i] -= 1;
-        }
-        board[row][col] += 1;
-    }
 }
