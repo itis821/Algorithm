ea0be3e5c24d0e066cc8ba8b8a9b85033d1c25c7
Lee Jisu <itis821@ajou.ac.kr>
2024-01-02 14:54:40 +0900

    BOJ1912

--- dp/BOJ1912.java
+++ dp/BOJ1912.java
@@ -3,17 +3,29 @@ package dp;
 import java.io.*;
 import java.util.*;
 
-public class BOJ1912 {
+class BOJ1912 {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine()); // 1<= n <= 100,000
-        int[] arr = new int[n];
+        int ans = Integer.MIN_VALUE;
 
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine()); // 1 <= n <= 100,000
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] numArr = new int[n]; // -1,000 <= numArr[i] <= 1,000
+        int[] dp = new int[n];
+
         for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+            numArr[i] = Integer.parseInt(st.nextToken());
         }
 
-    }
+        dp[0] = numArr[0];
+        ans = dp[0];
 
+        for (int i = 1; i < n; i++) {
+            dp[i] = Math.max(dp[i - 1] + numArr[i], numArr[i]);
+            ans = Math.max(ans, dp[i]);
+        }
+
+        System.out.println(ans);
+    }
 }
